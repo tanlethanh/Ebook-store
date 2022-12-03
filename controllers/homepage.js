c051e8a9d8fdbf500f5e8061defc54c0030a9df6
TheVerzius <nguyenmanh.4949@gmail.com>
2022-12-03 19:54:37 +0700

    add filter + fix sub menu

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,7 +1,33 @@
-const { getAllBooks, getAllCategories } = require('../models/books')
+const { 
+    getAllBooks,
+    getAllCategories,
+    getBooksBySpecCategoryId,
+    getSpecCategoryNameById,
+    getBooksByCategoryName
+} = require('../models/books')
 
 exports.renderHomepage = async (req, res) => {
     const books = await getAllBooks()
     const categories = await getAllCategories()
     res.render('home', { books: books, categories: categories })
+}
+
+exports.renderHomeWithFilter = async (req, res) => {
+    const categories = await getAllCategories()
+    const category = req.params.cname;
+    
+    if (req.params.scid)
+    {
+        const books = await getBooksBySpecCategoryId(req.params.scid)
+        Promise.resolve(getSpecCategoryNameById(req.params.scid))
+            .then(sc => {
+                console.log(sc)
+                res.render('home', { books: books, categories: categories, title: `Tất cả sách/${category}/${sc[0].name}` });
+            })
+    }
+    else
+    {
+        const books = await getBooksByCategoryName(req.params.cname)
+        res.render('home', { books: books, categories: categories, title: `Tất cả sách/${category}` });
+    }
 }
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { renderHomepage } = require('../controllers/homepage')
+const { renderHomepage, renderHomeWithFilter } = require('../controllers/homepage')
 
 router = express.Router()
 
 module.exports = router
     .get('/', renderHomepage)
 
+module.exports = router
+    .get('/home/:cname/:scid', renderHomeWithFilter)
 
+module.exports = router
+    .get('/home/:cname', renderHomeWithFilter)
